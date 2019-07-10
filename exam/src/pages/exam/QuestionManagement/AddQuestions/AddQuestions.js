f8319e0882effc6d102390d8f5c4e278ed0ece43
God <zx13835973692@163.com>
2019-07-10 21:38:48 +0800

    查看试题排版

--- exam/src/pages/exam/QuestionManagement/AddQuestions/AddQuestions.js
+++ exam/src/pages/exam/QuestionManagement/AddQuestions/AddQuestions.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
+import Editor from 'for-editor'
 
 function AddQuestion() {
   return (
     <div>
       添加试题
+      <Editor style={{height:200}}></Editor>
     </div>
   );
 }
