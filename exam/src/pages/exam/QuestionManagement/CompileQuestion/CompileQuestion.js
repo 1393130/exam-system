e6e79636f54192564241aec05acbe3e50992b6d2
AL <l641152742@163.com>
2019-07-11 23:34:27 +0800

    详情页排版

---
+++ exam/src/pages/exam/QuestionManagement/CompileQuestion/CompileQuestion.js
@@ -0,0 +1,194 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'dva';
+import Editor from 'for-editor'
+import styles from './CompileQuestion.css'
+import { Form, Input, Select, Button, Modal,message } from 'antd';
+
+function CompileQuestion(props) {
+  if(props.addInfo==1){
+    message.success('修改成功')
+  }
+  console.log(props)
+  useEffect(() => {
+    props.getExamType()//获取考试类型
+    props.getSubject()//获取课程类型
+    props.getTopicType()//获取题目类型
+    props.getUserInfo()//获取用户信息
+  }, [])
+  //控制弹框
+  const [visible, change_visible] = useState(false);
+  let showModal = () => {
+    change_visible(true)
+  };
+  let handleOk = e => {
+    change_visible(false)
+    handleSubmit()
+  };
+
+  let handleCancel = e => {
+    change_visible(false)
+  };
+  //从form中校验
+  const { getFieldDecorator } = props.form;
+  //处理表单提交
+  let handleSubmit = () => {
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        let examID = props.examType.find(item => item.exam_name === values.exam_id).exam_id;
+        let subjectID = props.subjectType.find(item => item.subject_text === values.subject_id).subject_id;
+        if(values.questions_type_id==="简答题"){
+          values.questions_type_id="774318-730z8m"
+        }
+        values.exam_id = examID;
+        values.subject_id = subjectID;
+        values.user_id = props.userInfo.user_id;
+        props.addQuestion(values)
+      }
+    });
+  };
+  const { Option } = Select;
+  return (
+    <div className={styles.addquestion}>
+      <h2>编辑试题</h2>
+      <section className={styles.addquestion_cont}>
+        <Form onSubmit={handleSubmit}>
+          <h4>题目信息</h4>
+          <div className={styles.question_stem}>
+            <h4>题干</h4>
+            <Form.Item>
+              {getFieldDecorator('title', {
+              })(
+                <Input
+                  placeholder="请输入题目标题，不超过20个字"
+                />,
+              )}
+            </Form.Item>
+          </div>
+          <div className={styles.topic_theme}>
+            <h4>题目主题</h4>
+            <Form.Item>
+              {getFieldDecorator('questions_stem', {
+              })(
+                <Editor style={{ height: 330 }}></Editor>,
+              )}
+            </Form.Item>
+          </div>
+          <div className={styles.test_type}>
+            <h4>请选择考试类型：</h4>
+            <Form.Item>
+              {getFieldDecorator('exam_id', {
+                initialValue: "周考1"
+              })(
+                <Select style={{ width: 200 }}>
+                  {
+                    props.examType && props.examType.map((item, index) => {
+                      return <Option value={item.exam_name} key={item.exam_id}>{item.exam_name}</Option>
+                    })
+                  }
+                </Select>,
+              )}
+            </Form.Item>
+          </div>
+          <div className={styles.test_type}>
+            <h4>请选择课程类型：</h4>
+            <Form.Item>
+              {getFieldDecorator('subject_id', {
+                initialValue: "javaScript上"
+              })(
+                <Select style={{ width: 200 }}>
+                  {
+                    props.subjectType && props.subjectType.map((item, index) => {
+                      return <Option value={item.subject_text} key={item.subject_id}>{item.subject_text}</Option>
+                    })
+                  }
+                </Select>,
+              )}
+            </Form.Item>
+          </div>
+          <div className={styles.test_type}>
+            <h4>请选择题目类型：</h4>
+            <Form.Item>
+              {getFieldDecorator('questions_type_id', {
+                initialValue: "简答题"
+              })(
+                <Select style={{ width: 200 }}>
+                  {
+                    props.TopicType && props.TopicType.map((item, index) => {
+                      return <Option value={item.questions_type_id} key={item.questions_type_id}>{item.questions_type_text}</Option>
+                    })
+                  }
+                </Select>,
+              )}
+            </Form.Item>
+          </div>
+          <div className={styles.topic_theme}>
+            <h4>答案信息</h4>
+            <Form.Item>
+              {getFieldDecorator('questions_answer', {
+              })(
+                <Editor style={{ height: 330 }}></Editor>,
+              )}
+            </Form.Item>
+          </div>
+          {/* <Button type="primary" size="large" htmlType="submit">
+            提交
+        </Button> */}
+          <Button type="primary" onClick={showModal} type="primary" size="large">
+            提交
+        </Button>
+          <Modal
+            title="Basic Modal"
+            visible={visible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          >
+            <h2>确认要修改吗？</h2>
+          </Modal>
+        </Form>
+
+      </section>
+    </div>
+  );
+}
+
+CompileQuestion.propTypes = {
+};
+
+const mapToProps = state => {
+  return { ...state, ...state.getExamType, ...state.getSubject, ...state.getTopicType, ...state.getUserInfo,...state.addQuestion }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    //获取考试类型
+    getExamType: () => {
+      dispatch({
+        type: "getExamType/getExamType"
+      })
+    },
+    //获取课程类型
+    getSubject: () => {
+      dispatch({
+        type: "getSubject/getSubject"
+      })
+    },
+    //获取题目类型
+    getTopicType: () => {
+      dispatch({
+        type: "getTopicType/getTopicType"
+      })
+    },
+    //获取用户信息
+    getUserInfo: () => {
+      dispatch({
+        type: "getUserInfo/getUserInfo"
+      })
+    },
+    addQuestion:payload=>{
+      dispatch({
+        type: "addQuestion/addQuestion",
+        payload
+      })
+    }
+  }
+}
+export default connect(mapToProps, mapDispatchToProps)(Form.create()(CompileQuestion));
--- exam/src/pages/exam/detail/QuestionDetail.js
+++ exam/src/pages/exam/detail/QuestionDetail.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import { connect } from 'dva';
-
+import { Tag, Button, Select , Form } from 'antd';
+import styles from './QuestionDetail.css'
 function QuestionDetail(props) {
     console.log(props)
   return (
-    <div>
-        详细
+    <div className={styles.Detail_wrap}>
+        <div className={styles.Detail_left}>
+            <h3 className={styles.Detail_setter}>出题人: dingshaoshan</h3>
+            <div className={styles.item_left_cont}>
+                <h4>题目信息</h4>
+                <div className={styles.item_style}>
+                    <div className="ant-tag ant-tag-blue">代码补全</div>
+                    <div className="ant-tag ant-tag-geekblue">JavaScript上</div>
+                    <div className="ant-tag ant-tag-orange">周考1</div>
+                </div>
+            </div>
+            <div className={styles.Detail_cont}>
+                <h2>机器人归位</h2>
+                <p>在二维平面上，有一个机器人从原点(0,0)开始,给出它的移动顺序,判断这个机器人在完成移动后是否在（0,0）处借宿。</p>
+                <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左).如果机器人在完成所有动作后返回原点,则返回true,否则返回false</p>
+                <div className={styles.Detail_living}>
+                    <b>实例1：</b>
+                    <div className={styles.Detail_living_cont}>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                    </div>
+                </div>
+                <div className={styles.Detail_living}>
+                    <b>实例2：</b>
+                    <div className={styles.Detail_living_cont}>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                        <p>移动顺序由字符串表示.机器人的有效动作有r(右),l(左),l(左)和,l(左)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={styles.Detail_right}></div>
     </div>
   );
 }
