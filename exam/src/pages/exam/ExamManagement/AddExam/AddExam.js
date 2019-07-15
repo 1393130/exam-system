2fa13c581232b3128905ca8bca03303cd170ff26
God <zx13835973692@163.com>
2019-07-15 17:22:54 +0800

    添加考试版

--- exam/src/pages/exam/ExamManagement/AddExam/AddExam.js
+++ exam/src/pages/exam/ExamManagement/AddExam/AddExam.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { connect } from 'dva';
+import styles from './AddExam.scss'
+import { Form, Input, Button } from 'antd';
 
-function AddExam() {
+function AddExam(props) {
+    const { getFieldDecorator } = props.form;
+    let handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      };
     return (
-        <div>
-            添加考试
-    </div>
+        <div className={styles.AddExam}>
+            <h2>添加考试</h2>
+            <div className={styles.addexam_cont}>
+                <Form onSubmit={handleSubmit} className="login-form">
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input
+                            placeholder="Username"
+                        />,
+                    )}
+                </Form>
+            </div>
+        </div>
     );
 }
 
