d9f5c65ecc96ce4d0d5262f1bdf01d79075a74e5
God <zx13835973692@163.com>
2019-07-17 17:18:48 +0800

    更新本地代码

--- exam/src/lang/en-US.js
+++ exam/src/lang/en-US.js
@@ -1,25 +1,25 @@
 export default {
-    'router.questions': 'Questions',
-    'router.questions.add': 'AddQuestions',
-    'router.questions.view': 'ViewQuestions',
-    'router.questions.type': 'ClassifyQuetions',
-  
-    'questions.type.title': 'ClassifyQuetions',
+  'router.questions': 'Questions',
+  'router.questions.add': 'AddQuestions',
+  'router.questions.view': 'ViewQuestions',
+  'router.questions.type': 'ClassifyQuetions',
 
-    'router.UserManage': 'UserManagement',
-    'router.UserManage.adduser': 'AddUser',
-    'router.UserManage.userdisplay': 'UserDisplay',
+  'questions.type.title': 'ClassifyQuetions',
 
-    'router.ExamManage':'ExamManagement',
-    'router.ExamManage.addexam':'AddExam',
-    'router.ExamManage.examlist':'ExamLists',
+  'router.UserManage': 'UserManagement',
+  'router.UserManage.adduser': 'AddUser',
+  'router.UserManage.userdisplay': 'UserDisplay',
 
-    'router.ClassManage':'ClassManagement',
-    'router.ClassManage.classmanage':'ClassManage',
-    'router.ClassManage.classroommanage':'ClassroomManage',
-    'router.ClassManage.studentmanage':'StudentManage',
+  'router.ExamManage': 'ExamManagement',
+  'router.ExamManage.addexam': 'AddExam',
+  'router.ExamManage.examlist': 'ExamLists',
 
-    'router.MarkManage':'MarkManagement',
-    'router.MarkManage.markmanage':'MarkManage'
-  
-  }
+  'router.ClassManage': 'ClassManagement',
+  'router.ClassManage.classmanage': 'ClassManage',
+  'router.ClassManage.classroommanage': 'ClassroomManage',
+  'router.ClassManage.studentmanage': 'StudentManage',
+
+  'router.MarkManage': 'MarkManagement',
+  'router.MarkManage.markmanage': 'MarkManage'
+
+}
